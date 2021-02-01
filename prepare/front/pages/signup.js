c6290aa3b2dfcb00a6e02a6673426e7bac2e10d9
kmlee95 <kmlee95@naver.com>
2021-02-01 23:17:45 +0900

    init commit

---
+++ prepare/front/pages/signup.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Head from 'next/head';
+
+const signup = () => {
+  return (
+    <>
+      <Head>
+        <title>회원가입 | NodeBird</title>
+      </Head>
+      <div>회원가입 페이지</div>
+    </>
+  );
+};
+
+export default signup;
