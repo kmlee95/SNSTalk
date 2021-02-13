2f4a817712f414f7006c17946884bf711c73adfd
kmlee95 <kmlee95@naver.com>
2021-02-13 21:00:00 +0900

    refactor : reducer separation

--- prepare/front/reducers/index.js
+++ prepare/front/reducers/index.js
@@ -1,56 +1,22 @@
 import { HYDRATE } from 'next-redux-wrapper';
-
-const initialState = {
-  user: {
-    isLoggedIn: false,
-    user: null,
-    signUpdata: {},
-    loginData: {},
-  },
-  post: {
-    mainPosts: [],
-  },
-};
-
-//acton creator
-export const loginAction = (data) => {
-  return {
-    type: 'LOG_IN',
-    data,
-  };
-};
-export const logoutAction = () => {
-  return {
-    type: 'LOG_OUT',
-  };
-};
+import user from './user';
+import post from './post';
+import { combineReducers } from 'redux';
 
 // 이전상태와 action을 통해 => 다음상태로
-const rootReducer = (state = initialState, action) => {
-  switch (action.type) {
-    case HYDRATE:
-      return { ...state, ...action.payload };
-    case 'LOG_IN':
-      return {
-        ...state,
-        user: {
-          ...state.user,
-          isLoggedIn: true,
-          user: action.data,
-        },
-      };
-    case 'LOG_OUT':
-      return {
-        ...state,
-        user: {
-          ...state.user,
-          isLoggedIn: false,
-          user: null,
-        },
-      };
-    default:
-      return state;
-  }
-};
+const rootReducer = combineReducers({
+  //HYDRATE를 위해 index reducer추가(서버사이드렌더링을 위해)
+  index: (state = {}, action) => {
+    switch (action.type) {
+      case HYDRATE:
+        return { ...state, ...action.payload };
+
+      default:
+        return state;
+    }
+  },
+  user,
+  post,
+});
 
 export default rootReducer;
