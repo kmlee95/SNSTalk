51a4fd9c2de8560df6561960cb9e9355375b02ab
kmlee95 <kmlee95@naver.com>
2021-02-16 00:48:38 +0900

    feat: hashtag

--- prepare/front/components/PostCard.js
+++ prepare/front/components/PostCard.js
@@ -12,6 +12,7 @@ import { useSelector } from 'react-redux';
 import PostImages from './PostImages';
 import CommentForm from './CommentForm';
 import Link from 'next/link';
+import PostCardContent from './PostCardContent';
 
 const PostCard = ({ post }) => {
   const [commentFormOpened, setCommentFormOpened] = useState(false);
@@ -64,7 +65,7 @@ const PostCard = ({ post }) => {
         <Card.Meta
           avatar={<Avatar>{post.User.nickname[0]}</Avatar>}
           title={post.User.nickname}
-          description={post.content}
+          description={<PostCardContent postData={post.content} />}
         />
       </Card>
       {commentFormOpened && (
