231e763a978c21450dc395c07975a6842896cf5e
kmlee95 <kmlee95@naver.com>
2021-02-20 23:46:32 +0900

    feat : follow, unfollow

--- front/src/pages/index.js
+++ front/src/pages/index.js
@@ -7,7 +7,7 @@ import { LOAD_POSTS_REQUEST } from '../reducers/post';
 
 const Home = () => {
   const { me } = useSelector((state) => state.user);
-  const { mainPosts } = useSelector((state) => state.post);
+  const { mainPosts, hasMorePosts, loadPostsLoading } = useSelector((state) => state.post);
   const dispatch = useDispatch();
 
   useEffect(() => {
@@ -15,6 +15,23 @@ const Home = () => {
       type: LOAD_POSTS_REQUEST,
     });
   }, []);
+
+  //인피니티 스크롤링 react-virtualized 도 생각해서 써보자 ..!
+  useEffect(() => {
+    function onScroll() {
+      if (window.scrollY + document.documentElement.clientHeight > document.documentElement.scrollHeight - 300) {
+        if (hasMorePosts && !loadPostsLoading) {
+          dispatch({
+            type: LOAD_POSTS_REQUEST,
+          });
+        }
+      }
+    }
+    window.addEventListener('scroll', onScroll);
+    return () => {
+      window.removeEventListener('scroll', onScroll);
+    };
+  }, [hasMorePosts]);
 
   return (
     <AppLayout>
