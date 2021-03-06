5ed2b04313bfe807ea6ed74fd2ad6e62e9046433
kmlee95 <kmlee95@naver.com>
2021-03-06 20:34:08 +0900

    fix : model class change

--- back/models/user.js
+++ back/models/user.js
@@ -1,34 +1,39 @@
-module.exports = (sequelize, DataTypes) => {
-  const User = sequelize.define(
-    //mysql에 users로 저장
-    'User',
-    {
-      //id가 기본적으로 들어있음.
-      email: {
-        type: DataTypes.STRING(30),
-        allowNull: false, //필수
-        unique: true, //고유값
+const DataTypes = require('sequelize');
+const { Model } = DataTypes;
+
+module.exports = class User extends Model {
+  static init(sequelize) {
+    return super.init(
+      {
+        // id가 기본적으로 들어있다.
+        email: {
+          type: DataTypes.STRING(30), // STRING, TEXT, BOOLEAN, INTEGER, FLOAT, DATETIME
+          allowNull: false, // 필수
+          unique: true, // 고유한 값
+        },
+        nickname: {
+          type: DataTypes.STRING(30),
+          allowNull: false, // 필수
+        },
+        password: {
+          type: DataTypes.STRING(100),
+          allowNull: false, // 필수
+        },
       },
-      nickname: {
-        type: DataTypes.STRING(30),
-        allowNull: false,
+      {
+        modelName: 'User',
+        tableName: 'users',
+        charset: 'utf8',
+        collate: 'utf8_general_ci', // 한글 저장
+        sequelize,
       },
-      password: {
-        type: DataTypes.STRING(100),
-        allowNull: false,
-      },
-    },
-    {
-      charset: 'utf8',
-      collate: 'utf8_general_ci', //한글저장
-    },
-  );
-  User.associate = (db) => {
-    db.User.hasMany(db.Post); //사람이 포스트여러개 가능
+    );
+  }
+  static associate(db) {
+    db.User.hasMany(db.Post);
     db.User.hasMany(db.Comment);
     db.User.belongsToMany(db.Post, { through: 'Like', as: 'Liked' });
     db.User.belongsToMany(db.User, { through: 'Follow', as: 'Followers', foreignKey: 'FollowingId' });
     db.User.belongsToMany(db.User, { through: 'Follow', as: 'Followings', foreignKey: 'FollowerId' });
-  };
-  return User;
+  }
 };
